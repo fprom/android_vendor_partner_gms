27ab0b7b7c8625a842c12e15b152e0b7b348c5b5
Bjorn <bjorn@example.com>
2022-02-17 23:00:15 +0000

    Initial commit

---
+++ products/gms.mk
@@ -0,0 +1,17 @@
+# Product priv-app
+PRODUCT_PACKAGES += \
+    additional_repos.xml \
+    AuroraServices \
+    AuroraStore \
+    com.google.android.maps.xml \
+    com.google.android.maps.jar \
+    FakeStore \
+    FDroid \
+    FDroidPrivilegedExtension \
+    GmsCore \
+    GsfProxy \
+    microg-a5k \
+    IchnaeaNlpBackend \
+    NominatimGeocoderBackend \
+    nlpBackendDejavu
+
